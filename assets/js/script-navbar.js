6873dda7dc252f608ffe1dc7107fd4dca5942cc1
Ramon Ribeiro <ramonrib@gmail.com>
2024-01-11 08:37:54 -0300

    start web

---
+++ assets/js/script-navbar.js
@@ -0,0 +1,23 @@
+const toggleMenu = () => {
+  const navigation = document.querySelector(".navigation");
+
+  const burgerMenu = document.querySelector(".menu-icon");
+  const src = burgerMenu.getAttribute("src");
+
+  const isBurger = src === "assets/images/navbar/burger-menu.svg";
+  const iconName = isBurger
+    ? "assets/images/navbar/close.svg"
+    : "assets/images/navbar/burger-menu.svg";
+
+  burgerMenu.setAttribute("src", iconName);
+
+  if (!isBurger) {
+    navigation.classList.add("navigation--mobile--fadeout");
+    setTimeout(() => {
+      navigation.classList.toggle("navigation--mobile");
+    }, 300);
+  } else {
+    navigation.classList.remove("navigation--mobile--fadeout");
+    navigation.classList.toggle("navigation--mobile");
+  }
+};
